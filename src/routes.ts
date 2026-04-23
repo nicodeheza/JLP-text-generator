61244a9c0a766d451eedb95dcf2bf911b23febc9
nicodeheza <nicolasdeheza@hotmail.com>
2026-04-23 21:02:24 +0200

    feat(user): register /user routes in main router

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 import generatorRoutes from './generator/routes.generator.js'
 import analyzerRoutes from './analyzer/routes.analyzer.js'
+import userRoutes from './user/routes.user.js'
 
 const router = Router()
 
 router.use('/generate', generatorRoutes)
 router.use('/analyze', analyzerRoutes)
+router.use('/user', userRoutes)
 
 export default router
