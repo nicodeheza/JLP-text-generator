fc760157086579bb70462a62ab52eab5f17bdd3d
nicodeheza <nicolasdeheza@hotmail.com>
2026-05-23 00:16:33 +0200

    icons

---
+++ frontend/src/components/Icon/Icons.component.tsx
@@ -0,0 +1,43 @@
+import type {FC} from 'react'
+import type {IconType} from 'react-icons/lib'
+import {PiGear} from 'react-icons/pi'
+import {FaGithub} from 'react-icons/fa'
+import {IoMoonOutline} from 'react-icons/io5'
+import {IoSunnyOutline} from 'react-icons/io5'
+import {BsStars} from 'react-icons/bs'
+import {MdOutlineInsertChart} from 'react-icons/md'
+import {FaRegTrashAlt} from 'react-icons/fa'
+import {BsLightningCharge} from 'react-icons/bs'
+import {MdOutlineUploadFile} from 'react-icons/md'
+
+type IconName =
+	| 'gear'
+	| 'github'
+	| 'moon'
+	| 'sun'
+	| 'stars'
+	| 'chart'
+	| 'trash'
+	| 'lightning'
+	| 'upload'
+
+const icons: Record<IconName, IconType> = {
+	gear: PiGear,
+	github: FaGithub,
+	moon: IoMoonOutline,
+	sun: IoSunnyOutline,
+	stars: BsStars,
+	chart: MdOutlineInsertChart,
+	trash: FaRegTrashAlt,
+	lightning: BsLightningCharge,
+	upload: MdOutlineUploadFile
+}
+
+interface Props {
+	icon: IconName
+}
+
+export const Icon: FC<Props> = ({icon}) => {
+	const IconComponent = icons[icon]
+	return <IconComponent />
+}
